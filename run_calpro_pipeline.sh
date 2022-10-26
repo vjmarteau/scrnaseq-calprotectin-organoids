0ef8701bd33c44eb19657433dcbdfeb81689b88f
vjmarteau <valentinmarteau7@gmail.com>
2022-10-26 15:21:25 +0200

    Remove path to workDir

--- run_calpro_pipeline.sh
+++ run_calpro_pipeline.sh
@@ -2,5 +2,4 @@
 # Run scRNAseq organoids calprotectin analysis pipeline
 
 nextflow run ./main.nf -profile cluster \
--w /data/scratch/marteau/nf-work-dir/nf-scRNAseq-organoids-calprotectin/work \
 -resume
